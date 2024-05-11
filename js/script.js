791d2aec5250b2569e672008570f9bf20caee442
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-11 10:10:40 -0700

    add function to change data in cards

--- js/script.js
+++ js/script.js
@@ -6,15 +6,25 @@ let currentTimeFrame = timeOption.id;
 for (const timeOption of timeOptions) {
     timeOption.addEventListener('change', (e) => {
         currentTimeFrame = e.currentTarget.id;
+        repopulateCards();
     })
 }
 
 window.addEventListener('load', () => { 
+    repopulateCards();
+})
+
+function repopulateCards() {
     
     for (const info of data) {
         const title = info['title'].replaceAll(new RegExp('\\s+', 'g'), '-').toLowerCase();
         const card = document.querySelector(`.card.${title}`);
+        const timePara = card.querySelector('.time');
+        const prevTimePara = card.querySelector('.prev-time');
+        const timeFramesObj = info.timeframes[`${currentTimeFrame}`];
         
-        
+        timePara.textContent = `${timeFramesObj.current}hrs`;
+        prevTimePara.textContent = `Last ${currentTimeFrame} - ${timeFramesObj.previous}hrs`;
+    
     }
-})
+}
