9133d79b92640b4ff3988748c561127416a9d002
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-10 22:59:13 -0700

    code for radio event listener

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 import data from '../data.json' with {type: 'json'};
+const timeOption = document.querySelector('.time-option:checked');
+const timeOptions = document.querySelectorAll('.time-option');
+let currentTimeFrame = timeOption.id;
+
+for (const timeOption of timeOptions) {
+    timeOption.addEventListener('change', (e) => {
+        currentTimeFrame = e.currentTarget.id;
+    })
+}
 
 window.addEventListener('load', () => { 
-    console.log(document.querySelector('.time-option:checked').id);
+    console.log(currentTimeFrame);
     for (const info of data) {
         console.log(info);
     }
